f20a7d274e5f0a447ac7cc6b2eff3c2650efc72f
David Stoline <drupal@davidstoline.com>
2020-01-21 14:37:35 +0100

    Update ddev-live.rb

--- Formula/ddev-live.rb
+++ Formula/ddev-live.rb
@@ -1,7 +1,7 @@
 class DdevLive < Formula
   desc "The command-line client for the DDEV Live platform"
   homepage "https://ddev.com"
-  url ""https://downloads.ddev.com/ddev-live-cli/v0.3.0/linux/ddev-live.zip"
+  url "https://downloads.ddev.com/ddev-live-cli/v0.3.0/linux/ddev-live.zip"
   version "0.3.0"
   sha256 "05fbdc831ac947085e2e742cf09967ac2bca0cea84f31c7895ab2922437f574c"
 
