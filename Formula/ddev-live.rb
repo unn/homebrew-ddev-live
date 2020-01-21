bb6b8bd04baf879acb9ee4a2c1af1027f3be04a0
David Stoline <drupal@davidstoline.com>
2020-01-21 14:34:09 +0100

    Update ddev-live.rb

--- Formula/ddev-live.rb
+++ Formula/ddev-live.rb
@@ -6,7 +6,7 @@ class DdevLive < Formula
   sha256 "bc8318e2b0c0ff21fba913ec3b89a2637476016417be8530c002abf449c5fe0e"
 
   bottle do
-    root_url "https://downloads.ddev.com/ddev-live-cli/v0.3.0/"
+    root_url "https://downloads.ddev.com/ddev-live-cli/v0.3.0/linux/ddev-live.zip"
     cellar :any_skip_relocation
     sha256 "05fbdc831ac947085e2e742cf09967ac2bca0cea84f31c7895ab2922437f574c" => :x86_64_linux
   end
